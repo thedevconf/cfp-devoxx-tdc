2994e009479d4980f843f72c3bf1953e9e1755d6
kleber <kleber@globalcode.com.br>
2017-11-22 22:18:02 -0200

    feat(proposal): remove email notification when a proposal is marked backup

--- app/controllers/ApproveOrRefuse.scala
+++ app/controllers/ApproveOrRefuse.scala
@@ -43,13 +43,10 @@ object ApproveOrRefuse extends SecureCFPController {
   /**
     * Allows a trackleader to change the state of a proposal to backup.
     *
-    * When the status is changed a notification email is sent to the Speaker
-    *
     */
   def backup(proposalId: String) = SecuredAction(IsMemberOf("cfp")){
     implicit request: SecuredRequest[play.api.mvc.AnyContent] =>
       Proposal.changeProposalState(request.webuser.uuid, proposalId, ProposalState.BACKUP)
-      notifyBackup(request.webuser.uuid,proposalId)
       Redirect(routes.CFPAdmin.allVotes("all", None)).flashing("success" -> ("Changed state to Backup"))
   }
 
@@ -121,13 +118,6 @@ object ApproveOrRefuse extends SecureCFPController {
       }
       Redirect(routes.ApproveOrRefuse.allApprovedByTalkType(talkType)).flashing("success" -> s"Notified speakers for Proposal ID $proposalId")
   }
-
-  def notifyBackup(reporterUUID: String, proposalId: String) =  {
-      Proposal.findById(proposalId).map {
-        proposal: Proposal =>
-          ZapActor.actor ! ProposalBackup(reporterUUID, proposal)
-      }
-   }
 
   def notifyRefused(talkType: String, proposalId: String) = SecuredAction(IsMemberOf("cfp")) {
     implicit request: SecuredRequest[play.api.mvc.AnyContent] =>
