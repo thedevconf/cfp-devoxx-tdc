083a64ba380eb99c0364b070397aeac465304a63
EC2 Default User <ec2-user@ip-10-0-0-41.us-west-2.compute.internal>
2021-02-06 13:41:17 +0000

    Fixed profile form

--- app/controllers/CallForPaper.scala
+++ app/controllers/CallForPaper.scala
@@ -1,26 +1,3 @@
-/*
- * The MIT License (MIT)
- *
- * Copyright (c) 2013 Association du Paris Java User Group.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of
- * this software and associated documentation files (the "Software"), to deal in
- * the Software without restriction, including without limitation the rights to
- * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
- * the Software, and to permit persons to whom the Software is furnished to do so,
- * subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
- * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
- * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
- * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
- * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
-
 package controllers
 
 import library.search.ElasticSearch
@@ -128,11 +105,11 @@ object CallForPaper extends SecureCFPController {
     "race" -> optional(text),
     "disability" -> optional(text),
     "socialMedia" -> mapping(
-      "twitter" -> optional(text.verifying(twitterURL)),
-      "linkedIn" -> optional(text.verifying(linkedInURL)),
-      "github" -> optional(text.verifying(githubURL)),
-      "facebook" -> optional(text.verifying(facebookURL)),
-      "instagram" -> optional(text.verifying(instagramURL))
+      "twitter" -> optional(text),
+      "linkedIn" -> optional(text),
+      "github" -> optional(text),
+      "facebook" -> optional(text),
+      "instagram" -> optional(text)
     )(SocialMedia.apply)(SocialMedia.unapply)
   )(Speaker.createSpeaker)(Speaker.unapplyForm))
 
--- app/library/S3.scala
+++ app/library/S3.scala
@@ -14,35 +14,38 @@ import collection.JavaConverters._
 object S3 {
 
   val bucketName = Play.current.configuration.getString("s3.bucket").getOrElse("thedevconf")
+  val bucketRegion = Play.current.configuration.getString("s3.region").getOrElse("sa-east-1")
+  
+  val s3 =  AmazonS3ClientBuilder.standard()
+      .withRegion(bucketRegion)
+      .build()
+  
   val presentationSourceDir = "/tmp/presentations/"
   val pictureSourceDir = "/tmp/photos/"
 
   def uploadPresentation(eventCode:String, trackId:String, filename:String) = {
-    val s3 = AmazonS3ClientBuilder.defaultClient()
     val key = s"presentations/$eventCode/$trackId/$filename"
     val putObject = new PutObjectRequest(bucketName,key,new File(presentationSourceDir + filename))
                         .withCannedAcl(CannedAccessControlList.PublicRead)
     s3.putObject(putObject)
-    s"https://s3-sa-east-1.amazonaws.com/$bucketName/$key"
+    s"https://s3-${bucketRegion}.amazonaws.com/$bucketName/$key"
   }
   
   def uploadPicture(userId:String, filename:String):String = {
-    val s3 = AmazonS3ClientBuilder.defaultClient()
     val extension = filename.substring(filename.indexOf(".")).toLowerCase
     val key = s"photos/$userId$extension"
     val putObject = new PutObjectRequest(bucketName,key,new File(pictureSourceDir + filename))
                         .withCannedAcl(CannedAccessControlList.PublicRead)
     s3.putObject(putObject)
-    s"https://s3-sa-east-1.amazonaws.com/$bucketName/$key"
+    s"https://s3-${bucketRegion}.amazonaws.com/$bucketName/$key"
   }
   
   /**
    * returns the URLS for the uploaded presentations for the specified event
    */
   def getUploadedPresentations(eventCode:String):List[String] = {
-    val s3 = AmazonS3ClientBuilder.defaultClient()
 	val result:ListObjectsV2Result = s3.listObjectsV2(bucketName,s"presentations/$eventCode")
 	val objects = result.getObjectSummaries().asScala
-	objects.map(o => s"https://s3-sa-east-1.amazonaws.com/$bucketName/${o.getKey}").toList
+	objects.map(o => s"https://s3-${bucketRegion}.amazonaws.com/$bucketName/${o.getKey}").toList
   }
 }
